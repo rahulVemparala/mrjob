de11387400f5acfb5fa3bf0dfc52f15229795207
Rahul Reddy Vemparala <111244611+rahulVemparala@users.noreply.github.com>
2023-04-01 16:08:51 -0400

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,17 +1,6 @@
+
 import sys
 
-total = 0
-num = 0
 for line in sys.stdin:
-    nums = line.strip().split()
-    if nums:
-        nums = [int(x) for x in nums]
-        total += sum(nums)
-        nums += len(nums)
-
-    else:
-        print("breaking ")
-        break
-
-# print(f"total: {sum(nums)}, num of observations : {len(nums)}")
-print '%d\t%d' % (sum(nums), len(nums))
+    num = float(line.strip())
+    print("{0}\t{1}".format(num, 1))
