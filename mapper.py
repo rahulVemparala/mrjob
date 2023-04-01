8a3fc6f8816cdf57b447dfb541ac26950a168fbd
Rahul Reddy Vemparala <111244611+rahulVemparala@users.noreply.github.com>
2023-04-01 19:38:28 -0400

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,6 +1,17 @@
-
+#!/usr/bin/env python
 import sys
 
+local_sum = 0
+local_n = 0
+
 for line in sys.stdin:
-    num = float(line.strip())
-    print("{0}\t{1}".format(num, 1))
+    nums = line.strip().split()
+    if nums:
+        nums = [int(x) for x in nums]
+        local_sum += sum(nums)
+        local_n += len(nums)
+    else:
+        break
+
+
+print "%d\t%d" % (local_sum, local_n)
